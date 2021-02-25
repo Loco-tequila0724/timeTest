d45bd068b347c7ced1ab9f594caffe1bda020e9b
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-02-25 18:09:34 +0900

    ３

--- timeTest/ViewController.swift
+++ timeTest/ViewController.swift
@@ -21,53 +21,95 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        dateData(country: germanyTime, language: "de_DE")
-//        dateJapan()
-//        dateTokyo()
 
-        dateData(country: tokyoTime, language: "jp_JP")
-//        overseas(country: nyTime, whereTo: "America/New_York" )
-        overseasTime()
+        dateData(country: tokyoTime, language: "ja_JP")
+        overseas(country: nyTime, lunguage: "en_US", whereTo: "America/New_York")
+        overseas(country: germanyTime, lunguage: "fr_GN", whereTo: "Europe/Berlin")
     }
-
 
     func dateData(country: UILabel, language: String) {
         let date = Date()
-        let dateformatter = DateFormatter()
-        dateformatter.dateStyle = .full
-        dateformatter.timeStyle = .short
-        let time = dateformatter.string(from: date)
-        dateformatter.locale = Locale(identifier: language )
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("yMMMdE jm")
+        formatter.locale = Locale(identifier: language)
+        let time = formatter.string(from: date)
         country.adjustsFontSizeToFitWidth = true
         country.text = "\(time)"
     }
 
-    func overseas(country:UILabel, whereTo:String) {
+    func overseas(country: UILabel, lunguage: String, whereTo: String) {
         let date = Date()
         let formatter = DateFormatter()
         formatter.setLocalizedDateFormatFromTemplate("yMMMdE jm")
-        if let timeZone = TimeZone(identifier: whereTo ) {
+        formatter.locale = Locale(identifier: lunguage)
+        if let timeZone = TimeZone(identifier: whereTo) {
             formatter.timeZone = timeZone
         }
         let time = formatter.string(from: date)
+        country.adjustsFontSizeToFitWidth = true
         country.text = time
     }
 
+    var timer = Timer()
+    var timeCount = 0
 
-    func overseasTime() {
-        let date = Date()
-        let formatter = DateFormatter()
-
-        formatter.setLocalizedDateFormatFromTemplate("yMMMdE jm")
-        if let nyTimeZone = TimeZone(identifier: "America/New_York") {
-            formatter.timeZone = nyTimeZone
+    @IBAction func startButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+        startButton.isEnabled = false
     }
 
-        let time = formatter.string(from: date)
-
-        nyTime.text = time
+    @IBAction func stopButton(_ sender: Any) {
+        timer.invalidate()
+        startButton.isEnabled = true
 
     }
+
+    @IBAction func resetButton(_ sender: Any) {
+        timer.invalidate()
+        timeCount = 0
+        watchTime.text = "0"
+    }
+
+    @objc func Action() {
+        timeCount += 1
+        watchTime.text = String(timeCount)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    func overseasTime() {
+//        let date = Date()
+//        let formatter = DateFormatter()
+//        formatter.setLocalizedDateFormatFromTemplate("yMMMdE jm")
+//
+////                             ↓↓　ここに各国のタイムゾーンIDを記入　↓↓
+//        if let nyTimeZone = TimeZone(identifier: "America/New_York") {
+//            formatter.timeZone = nyTimeZone
+//        }
+//
+//        let time = formatter.string(from: date)
+//        nyTime.text = time
+//
+//    }
 //        let date = Date()
 //        let formatter = DateFormatter()
 //        formatter.setLocalizedDateFormatFromTemplate("yMMMMdE jm")
@@ -76,65 +118,65 @@ class ViewController: UIViewController {
 //            formatter.timeZone = timeZone
 //            print(formatter.string(from: date))
 
-            // Wed, May 30, 2018, 11:47 AM
-            // 2018年5月30日(水) 11:51
-}
+// Wed, May 30, 2018, 11:47 AM
+// 2018年5月30日(水) 11:51
 
 
 
-        ////        dateData(country: tokyoTime, language: "ja_JP")
-        //        dateData(country: nyTime, language: "us_US")
-        //        dateData(country: germanyTime, language: "de_DE")
+
+////        dateData(country: tokyoTime, language: "ja_JP")
+//        dateData(country: nyTime, language: "us_US")
+//        dateData(country: germanyTime, language: "de_DE")
 
 
 
-        //    struct timeDate {
-        //        let date = Date()
-        //        let formatter = DateFormatter()
-        //        let label:UILabel
-        //
-        //        init(label:UILabel) {
-        //            self.label = label
-        //        }
-        //
-        //        func styleStyle(label:UILabel) {
-        //            formatter.dateStyle = .full
-        //            formatter.timeStyle = .full
-        //            let time = formatter.string(from: date)
-        //            label.text = time
-        //        }
-        //
-        //        func styleTemplate(label:UILabel) {
-        //            formatter.setLocalizedDateFormatFromTemplate("yMMdEEEE jm")
-        //            let time = formatter.string(from: date)
-        //            label.text = time
-        //        }
-        //    }
-        //
-        //    var dateJapan = timeDate(label: japanTime)
+//    struct timeDate {
+//        let date = Date()
+//        let formatter = DateFormatter()
+//        let label:UILabel
+//
+//        init(label:UILabel) {
+//            self.label = label
+//        }
+//
+//        func styleStyle(label:UILabel) {
+//            formatter.dateStyle = .full
+//            formatter.timeStyle = .full
+//            let time = formatter.string(from: date)
+//            label.text = time
+//        }
+//
+//        func styleTemplate(label:UILabel) {
+//            formatter.setLocalizedDateFormatFromTemplate("yMMdEEEE jm")
+//            let time = formatter.string(from: date)
+//            label.text = time
+//        }
+//    }
+//
+//    var dateJapan = timeDate(label: japanTime)
 
-        //
-        //
-        //    func dateJapan() {
-        //        let date = Date()
-        //        let formatter = DateFormatter()
-        //        formatter.setLocalizedDateFormatFromTemplate("yMMMMdEEEE jm")
-        //
-        //        let time = formatter.string(from: date)
-        //        japanTime.text = time
-        //    }
-        //
-        //
-        //    func dateTokyo() {
-        //        let date = Date()
-        //        let formatter = DateFormatter()
-        //        formatter.dateStyle = .full
-        //        formatter.timeStyle = .short
-        //        let time = formatter.string(from: date)
-        //        tokyoTime.text = time
-        //    }
-        //
-        //
+//
+//
+//    func dateJapan() {
+//        let date = Date()
+//        let formatter = DateFormatter()
+//        formatter.setLocalizedDateFormatFromTemplate("yMMMMdEEEE jm")
+//
+//        let time = formatter.string(from: date)
+//        japanTime.text = time
+//    }
+//
+//
+//    func dateTokyo() {
+//        let date = Date()
+//        let formatter = DateFormatter()
+//        formatter.dateStyle = .full
+//        formatter.timeStyle = .short
+//        let time = formatter.string(from: date)
+//        tokyoTime.text = time
+//    }
+//
+//
 
 
 
